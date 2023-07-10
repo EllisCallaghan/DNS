c86ac0a6cc095b39aec2c50d71b3834d50fd6dd1
EllisCallaghan <callaghanellis440@gmail.com>
2023-07-10 10:03:09 +0800

    10/7/2023

--- app/[id]/page.jsx
+++ app/[id]/page.jsx
@@ -1,4 +1,4 @@
-
+"use server"
 const dns = require("dns");
 const dnsPromises = dns.promises;
 
@@ -7,92 +7,112 @@ async function doLookupAll(domain){
 
 }
 async function doLookupA(domain){
-  try {
-    return dnsPromises.resolve4(domain)
-  } catch (error) {
-    return error
-  }
-  
+  return dnsPromises.resolve4(domain,{ttl:true}).then((item) =>{
+    return item
+  }).catch((error) => {
+    console.log('gm')
+    return []})
 }
 
+
 async function doLookupAAAA(domain){
-  try {
-    return dnsPromises.resolve6(domain)
-  } catch (error) {
-    return error
-  }}
+  
+  return dnsPromises.resolve6(domain,{ttl:true}).then((item) =>{
+    return item
+  }).catch((error) => {
+    console.log('no IpV6 Records')
+    return []})
+}
 
 async function doLookupCname(domain){
-  return dnsPromises.resolveCname(domain)
+  return dnsPromises.resolveCname(domain).then((item) =>{
+    return item
+  }).catch((error) => {
+    console.log('no Cname Records')
+    return []})
 }
 
 async function doLookupMx(domain){
-  return dnsPromises.resolveMx(domain)
+  return dnsPromises.resolveMx(domain).then((item) =>{
+    return item
+  }).catch((error) => {
+    console.log('no Mx Records')
+    return []})
 }
 
 async function doLookupTxt(domain){
-  return dnsPromises.resolveTxt(domain)
+  return dnsPromises.resolveTxt(domain).then((item) =>{
+    return item
+  }).catch((error) => {
+    console.log('no Txt Records')
+    return []})
 }
 
 async function doLookupNs(domain){
-  return dnsPromises.resolveNs(domain)
+  return dnsPromises.resolveNs(domain).then((item) =>{
+    return item
+  }).catch((error) => {
+    console.log('no Ns Records')
+    return []})
+  
 }
 async function doLookupSrv(domain){
-  return dnsPromises.resolveSrv(domain)
+  return dnsPromises.resolveSrv(domain).then((item) =>{
+    return item
+  }).catch((error) => {
+    console.log('no SRV Records')
+    return []})
 }
 async function doLookupCaa(domain){
-  return dnsPromises.resolveCaa(domain)
+  return dnsPromises.resolveCaa(domain).then((item) =>{
+    return item
+  }).catch((error) => {
+    console.log('no Caa Records')
+    return []})
 }
 
 async function doLookupSoa(domain){
-  return dnsPromises.resolveSoa(domain)
+  return dnsPromises.resolveSoa(domain).then((item) =>{
+    return item
+  }).catch((error) => {
+    console.log('no Soa Records')
+    return []})
 }
 
 async function doLookupPtr(domain){
-  return dnsPromises.resolvePtr(domain)
+  return dnsPromises.resolvePtr(domain).then((item) =>{
+    return item
+  }).catch((error) => {
+    console.log('no Ptr Records')
+    return []})
 }
 async function doLookupNaptr(domain){
-  return dnsPromises.resolveNaptr(domain)
+  dns.resolveNaptr(domain,(err,result) =>{
+    if(err){
+      //console.log(err)
+    }else{
+      console.log(result)
+      return result
+    }
+  })
 }
 export default async function Page({ params }) {
   
-
   const arr4 = await doLookupA(params.id)
-  if(arr4.length === 0){
-    arr4.push({address: "No Record",ttl: "No Record"})
-  };
-  
+
   const arr6 = await doLookupAAAA(params.id)
-  if(arr6.length === 0){
-    arr6.push({address: "No Record",ttl: "No Record"})
-  };
-
   const arrCNAME = await doLookupCname(params.id)
-  if(arrCNAME.length === 0){
-    arrCNAME.push({value:'no record'})
-  };
 
-  const arrMx = await await doLookupMx(params.id)
-  if(arrMx.length === 0){
-    arrCNAME.push({exchange:'No Record',priority:'No Record'})
-  };
+  const arrMx = await doLookupMx(params.id)
 
-  const arrTxt = await await doLookupTxt(params.id)
+  const arrTxt =  await doLookupTxt(params.id)
 
-  const arrNs = await await doLookupNs(params.id)
-  
-  const arrSrv = await await doLookupSrv(params.id)
-  if(arrSrv.length === 0){
-    arrSrv.push({priority:NaN,weight:NaN,port:NaN,name:"No Record"})
-  }
+  const arrNs =  await doLookupNs(params.id)
 
-  const arrCaa = await await doLookupCaa(params.id)
-  if(arrCaa.length === 0){
-    arrCaa.push({critical:NaN,issue:'No Record',iodef:'No Record'})
-  }
-
-  const arrSoa = await await doLookupSoa(params.id)
-
+  const arrSrv =  await doLookupSrv(params.id)
+  const arrCaa =await doLookupCaa(params.id)
+  const arrSoa =  await doLookupSoa(params.id)
+  const arrPtr = await doLookupPtr(params.id)
   
 
   return (
@@ -100,27 +120,35 @@ export default async function Page({ params }) {
       <p className="text-2xl">{params.id}</p>
       <div>
         <h1 className="text-2xl">IPv4 addresses</h1>
-        {
-        arr4.map((address) => (
-          <div>
-          <p >{address.address}</p>
-          <p >ttl:{address.ttl}</p>
-          </div>
-        ))}
+          
+            {arr4.map((item)=>(
+              <div>
+              <p >{item.address}</p>
+              <p >ttl:{item.ttl}</p>
+              </div>
+            ))
+
+            }
+          
+          
+
       </div>
       <div>
         <h1 className="text-2xl">IPv6 addresses</h1>
-        {
-        arr6.map((address) => (
-          <div>
-          <p >{address.address}</p>
-          <p >ttl:{address.ttl}</p>
-          </div>
-        ))}
+
+        {arr6 === undefined ? null : arr6.map((item)=>(
+              <div>
+              <p >{item.address}</p>
+              <p >ttl:{item.ttl}</p>
+              </div>
+            ))
+
+            }
+
       </div>
       <div>
         <h1 className="text-2xl">CNAME addresses</h1>
-        {
+        {arrCNAME === undefined ? null :
         arrCNAME.map((address) => (
           <div>
           <p >value: {address.value}</p>
@@ -129,49 +157,64 @@ export default async function Page({ params }) {
       </div>
       <div>
         <h1 className="text-2xl">TXT records</h1>
-        {arrTxt.map((address) => (
+
           <div>
-          <p>{address.entries}</p>
+          {arrTxt.map((item)=>(
+            <p>{item}</p>
+          ))}
           </div>
-        ))}
+
       </div>
       <div>
         <h1 className="text-2xl">NS records</h1>
-        {arrNs.map((address) => (
+
           <div>
-          <p>{address.value}</p>
+          <p>{arrNs}</p>
           </div>
-        ))}
+
       </div>
       <div>
       <h1 className="text-2xl">CAA records</h1>
-        { 
-        arrCaa.map((address) => (
+
           <div>
-          <p >{`critical: ${address.critical},  issue: ${address.issue}`}</p>
+          {
+            arrCaa.map((item) => (
+              <p >{`critical: ${item.critical},  issue: ${item.issue}`}</p>
+            ))
+          }
+          
           </div>
-        ))}
+
       </div>
       <div>
       <h1 className="text-2xl">MX records</h1>
-        {arrMx.map((address) => (
+
           <div>
-          <p >{`exchange: ${address.exchange},  priority: ${address.priority}`}</p>
+            {
+              arrMx.map((item) =>(
+                <p >{`exchange: ${item.exchange},  priority: ${item.priority}`}</p>
+              ))
+            }
+          
           </div>
-        ))}
+
       </div>
       <div>
       <h1 className="text-2xl">SOA records</h1>
           <div className="flex flex-col">
-            {arrSoa.map((address => (
-              <div>
-              <p>{`nsname: ${address.nsname},  hostmaster: ${address.hostmaster}`}</p>
-              <p>{`serial: ${address.serial},  refresh: ${address.refresh}`}</p>
-              <p>{`retry: ${address.retry},  expire: ${address.expire}`}</p>
-              <p>{`minttl:${address.minttl}`}</p>
+            <div>
+            
+              <p>{`nsname: ${arrSoa.nsname},  hostmaster: ${arrSoa.hostmaster}`}</p>
+              <p>{`serial: ${arrSoa.serial},  refresh: ${arrSoa.refresh}`}</p>
+              <p>{`retry: ${arrSoa.retry},  expire: ${arrSoa.expire}`}</p>
+              <p>{`minttl:${arrSoa.minttl}`}</p>
               </div>
-            )))
-          }
+            
+          
+
+          
+              
+
           </div>
       </div>
       <div>
@@ -181,6 +224,16 @@ export default async function Page({ params }) {
           <div>
             <p>{`priority:${address.priority}, weight:${address.weight}`}</p>
             <p>{`port:${address.port}, name:${address.name}`}</p>
+          </div>
+        ))
+      }
+      </div>
+      <div>
+      <h1>PTR records</h1>
+      {
+        arrPtr.map((item) =>(
+          <div>
+            <p>{item}</p>
           </div>
         ))
       }
